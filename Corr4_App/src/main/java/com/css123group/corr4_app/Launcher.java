7034efe13643754ac06c53bd28eb8b189b5b7adc
jcsxxvii <100681442+jcsxxvii@users.noreply.github.com>
2025-11-07 22:40:40 +0800

    Delete Corr4_App/src/main/java/com/css123group/corr4_app directory

--- Corr4_App/src/main/java/com/css123group/corr4_app/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.css123group.corr4_app;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(BankApp.class, args);
-    }
-}
